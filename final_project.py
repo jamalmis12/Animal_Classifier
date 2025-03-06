d3241f9eac60e00a3f74c7b0ccf6dfde310bd095
Jamal Naga <157566024+jamalmis12@users.noreply.github.com>
2025-03-06 11:55:36 +0800

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -13,6 +13,9 @@ st.set_page_config(
 # Custom CSS for dark mode and styling
 st.markdown("""
     <style>
+        .stApp {
+            background: linear-gradient(135deg, #1a1a1a, #252525);
+        }
         .title {
             text-align: center;
             font-size: 50px;
